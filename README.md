64fb423f3142a594df1461fe80bdf17578941838
LambdaXymox <lambda.xymox@gmail.com>
2020-10-31 13:56:51 -0500

    Add module documentation and fill out the readme file.

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Bitset Library
+# BitSet Library
+
+## Introduction
+**bitset** is a crate introducing a data type representing sets of bits.
+
+## Getting Started
+To use the library as a dependency in your project, Add **bitset** to your 
+`Cargo.toml` file
+
+```ignore
+[dependencies]
+bitset = "0.2.0"
+```
+
+After that, place the crate declaration in your project's `main.rs` 
+or `lib.rs` file
+
+```rust
+extern crate bitset;
+```
+
+## Features
+**bitset** is a library adds the `BitSet` data type to a system. This allows
+one to use a sequence of arbitrary many bits and apply standard logic operations
+to them. One possible use case would be for tracking data dependencies between 
+subsystems in a larger software system using the bitset to track what data 
+components each data type has a in data-oriented design fashion. One can apply
+the standard bitwise logic operations to bit sets such as logical AND, logical 
+OR, logical XOR, SHIFT LEFT, SHIFT RIGHT, and logical NEGATION. One can also 
+query, test, set and flip individual bits.
+
+## Limitations
+The main limitation of the bitset crate is that it only supports a bit set 
+capacity of 128 bits. This is the largest possible unsigned integer that Rust's
+type systems currently allows. This limitation will be removed in the future
+when Rust gets const generics.
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,39 @@
 # BitSet Library
 
 ## Introduction
-**bitset** is a crate 
+**bitset** is a crate introducing a data type representing sets of bits.
+
+## Getting Started
+To use the library as a dependency in your project, Add **bitset** to your 
+`Cargo.toml` file
+
+```ignore
+[dependencies]
+bitset = "0.2.0"
+```
+
+After that, place the crate declaration in your project's `main.rs` 
+or `lib.rs` file
+
+```rust
+extern crate bitset;
+```
+
+## Features
+**bitset** is a library adds the `BitSet` data type to a system. This allows
+one to use a sequence of arbitrary many bits and apply standard logic operations
+to them. One possible use case would be for tracking data dependencies between 
+subsystems in a larger software system using the bitset to track what data 
+components each data type has a in data-oriented design fashion. One can apply
+the standard bitwise logic operations to bit sets such as logical AND, logical 
+OR, logical XOR, SHIFT LEFT, SHIFT RIGHT, and logical NEGATION. One can also 
+query, test, set and flip individual bits.
+
+## Limitations
+The main limitation of the bitset crate is that it only supports a bit set 
+capacity of 128 bits. This is the largest possible unsigned integer that Rust's
+type systems currently allows. This limitation will be removed in the future
+when Rust gets const generics.
 */
 
 #![no_std]
@@ -13,7 +45,7 @@ use core::ops;
 
 
 /// A fixed-size sequence of N bits. Bitsets can be transformed by 
-/// standard logic operators and converted to and from strings and integers.
+/// standard logic operators and converted to and from integers.
 #[repr(C)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct BitSet {
