ddfb1e5e2e97a1cc531451ebcb572d51a6f93f8e
LambdaXymox <lambda.xymox@gmail.com>
2020-10-31 11:49:14 -0500

    Add operation implementations for bit sets.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 extern crate core;
 
 
+use core::ops;
+
+
 pub struct BitSet {
     data: u128,
 }
@@ -12,6 +15,20 @@ impl BitSet {
     pub const fn new() -> BitSet {
         BitSet {
             data: 0,
+        }
+    }
+
+    #[inline]
+    pub const fn from_u64(value: u64) -> BitSet {
+        BitSet {
+            data: value as u128
+        }
+    }
+
+    #[inline]
+    pub const fn from_u128(value: u128) -> BitSet {
+        BitSet {
+            data: value
         }
     }
 
@@ -105,6 +122,340 @@ impl BitSet {
 
     pub fn to_u128(&self) -> Option<u128> {
         Some(self.data)
+    }
+}
+
+impl ops::BitAnd<BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitand(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data & other.data;
+
+        bitset
+    }
+}
+
+impl ops::BitAnd<&BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitand(self, other: &BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data & other.data;
+
+        bitset
+    }
+}
+
+impl ops::BitAnd<BitSet> for &BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitand(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data & other.data;
+
+        bitset
+    }
+}
+
+impl<'a, 'b> ops::BitAnd<&'a BitSet> for &'b BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitand(self, other: &'a BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data & other.data;
+
+        bitset
+    }
+}
+
+impl ops::BitOr<BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitor(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data | other.data;
+
+        bitset
+    }
+}
+
+impl ops::BitOr<&BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitor(self, other: &BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data | other.data;
+
+        bitset
+    }
+}
+
+impl ops::BitOr<BitSet> for &BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitor(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data | other.data;
+
+        bitset
+    }
+}
+
+impl<'a, 'b> ops::BitOr<&'a BitSet> for &'b BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitor(self, other: &'a BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data | other.data;
+
+        bitset
+    }
+}
+
+impl ops::BitXor<BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitxor(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data ^ other.data;
+
+        bitset
+    }
+}
+
+impl ops::BitXor<&BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitxor(self, other: &BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data ^ other.data;
+
+        bitset
+    }
+}
+
+impl ops::BitXor<BitSet> for &BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitxor(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data ^ other.data;
+
+        bitset
+    }
+}
+
+impl<'a, 'b> ops::BitXor<&'a BitSet> for &'b BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn bitxor(self, other: &'a BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data ^ other.data;
+
+        bitset
+    }
+}
+
+impl ops::Shl<BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn shl(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data << other.data;
+
+        bitset
+    }
+}
+
+impl ops::Shl<&BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn shl(self, other: &BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data << other.data;
+
+        bitset
+    }
+}
+
+impl ops::Shl<BitSet> for &BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn shl(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data << other.data;
+
+        bitset
+    }
+}
+
+impl<'a, 'b> ops::Shl<&'a BitSet> for &'b BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn shl(self, other: &'a BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data << other.data;
+
+        bitset
+    }
+}
+
+impl ops::Shr<BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn shr(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data >> other.data;
+
+        bitset
+    }
+}
+
+impl ops::Shr<&BitSet> for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn shr(self, other: &BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data >> other.data;
+
+        bitset
+    }
+}
+
+impl ops::Shr<BitSet> for &BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn shr(self, other: BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data >> other.data;
+
+        bitset
+    }
+}
+
+impl<'a, 'b> ops::Shr<&'a BitSet> for &'b BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn shr(self, other: &'a BitSet) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = self.data >> other.data;
+
+        bitset
+    }
+}
+
+impl ops::BitAndAssign<BitSet> for BitSet {
+    #[inline]
+    fn bitand_assign(&mut self, other: BitSet) {
+        self.data &= other.data;
+    }
+}
+
+impl ops::BitAndAssign<&BitSet> for BitSet {
+    #[inline]
+    fn bitand_assign(&mut self, other: &BitSet) {
+        self.data &= other.data;
+    }
+}
+
+impl ops::BitOrAssign<BitSet> for BitSet {
+    #[inline]
+    fn bitor_assign(&mut self, other: BitSet) {
+        self.data |= other.data;
+    }
+}
+
+impl ops::BitOrAssign<&BitSet> for BitSet {
+    #[inline]
+    fn bitor_assign(&mut self, other: &BitSet) {
+        self.data |= other.data;
+    }
+}
+
+impl ops::BitXorAssign<BitSet> for BitSet {
+    #[inline]
+    fn bitxor_assign(&mut self, other: BitSet) {
+        self.data ^= other.data;
+    }
+}
+
+impl ops::BitXorAssign<&BitSet> for BitSet {
+    #[inline]
+    fn bitxor_assign(&mut self, other: &BitSet) {
+        self.data ^= other.data;
+    }
+}
+
+impl ops::ShlAssign<BitSet> for BitSet {
+    #[inline]
+    fn shl_assign(&mut self, other: BitSet) {
+        self.data <<= other.data;
+    }
+}
+
+impl ops::ShlAssign<&BitSet> for BitSet {
+    #[inline]
+    fn shl_assign(&mut self, other: &BitSet) {
+        self.data <<= other.data;
+    }
+}
+
+impl ops::ShrAssign<BitSet> for BitSet {
+    #[inline]
+    fn shr_assign(&mut self, other: BitSet) {
+        self.data <<= other.data;
+    }
+}
+
+impl ops::ShrAssign<&BitSet> for BitSet {
+    #[inline]
+    fn shr_assign(&mut self, other: &BitSet) {
+        self.data <<= other.data;
+    }
+}
+
+impl ops::Not for BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn not(self) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = !self.data;
+
+        bitset
+    }
+}
+
+impl ops::Not for &BitSet {
+    type Output = BitSet;
+
+    #[inline]
+    fn not(self) -> Self::Output {
+        let mut bitset = BitSet::new();
+        bitset.data = !self.data;
+
+        bitset
     }
 }
 
